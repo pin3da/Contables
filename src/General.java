d1ab666336227bf4aa325ed9d92dadb9cd5e8e9b
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2012-06-27 14:14:09 -0500

    Initial Commit

---
+++ src/General.java
@@ -0,0 +1,58 @@
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
+import java.util.LinkedList;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pin3da
+ */
+public class General {
+    Mongo m;
+    DB db;
+    DBCollection cuentas;
+    DBCollection ingre;
+    DBCollection gasto;
+    DBCollection activos;
+    DBCollection paypa;
+    
+    public General(Mongo m, String name){
+        this.m = m;
+        this.db = m.getDB(name);
+        this.cuentas = db.getCollection("cuentas");
+        this.ingre = db.getCollection("ingresos");
+        this.gasto = db.getCollection("gasto");
+        this.activos = db.getCollection("activos");
+        this.paypa = db.getCollection("paypa");
+    }
+
+    public LinkedList<BasicDBObject> listCuentas(){
+        DBCursor cur = cuentas.find();
+        LinkedList<BasicDBObject> results = new LinkedList<BasicDBObject>();
+        while(cur.hasNext())
+            results.add((BasicDBObject)cur.next());
+        return results;
+    }
+    
+    public boolean addCuenta(String id, String name){
+        BasicDBObject c = new BasicDBObject("id", id).append("name", name);
+        if (cuentas.findOne(c) == null){
+            cuentas.insert(c);
+            return true;
+        }
+        return false;
+    }
+    
+    public void modifyCuenta(String id, String name){
+        BasicDBObject update = new BasicDBObject("id", id).append("name", name);
+        cuentas.update(new BasicDBObject("id",id), update);
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+import com.mongodb.Mongo;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pin3da
+ */
+public class Test {
+    public static void main(String []Args){
+        Mongo mio = 
+    }
+}
